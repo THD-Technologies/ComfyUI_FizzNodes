4943996a410afc6ea66a6f54fe96c36efd3ee1ed
MaxTran96 <maxtran3005@gmail.com>
2023-10-23 00:03:57 -0700

    fixed __init__

--- __init__.py
+++ __init__.py
@@ -11,9 +11,12 @@ import __main__
 python = sys.executable
 
 
-extentions_folder = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
+# extentions_folder = os.path.join(os.path.dirname(os.path.realpath(__main__.__file__)),
+#                                  "web" + os.sep + "extensions" + os.sep + "FizzleDorf")
+extentions_folder = os.path.join("/home/ubuntu/content/ComfyUI/custom_nodes/ComfyUI_FizzNodes",
                                  "web" + os.sep + "extensions" + os.sep + "FizzleDorf")
-javascript_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "javascript")
+# javascript_folder = os.path.join(os.path.dirname(os.path.realpath(__file__)), "javascript")
+javascript_folder = os.path.join("/home/ubuntu/content/ComfyUI/custom_nodes/ComfyUI_FizzNodes", "javascript")
 
 if not os.path.exists(extentions_folder):
     print('Making the "web\extensions\FizzleDorf" folder')
